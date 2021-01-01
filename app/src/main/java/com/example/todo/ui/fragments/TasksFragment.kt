51113e628a8a9874a2a1971210ec511b66032ca3
Jvitaliy <jvit82@gmail.com>
2021-01-01 23:27:59 +0700

    add menu

--- app/src/main/java/com/example/todo/ui/fragments/TasksFragment.kt
+++ app/src/main/java/com/example/todo/ui/fragments/TasksFragment.kt
@@ -1,13 +1,18 @@
 package com.example.todo.ui.fragments
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.todo.R
 import com.example.todo.adapters.TaskAdapter
 import com.example.todo.databinding.FragmentTasksBinding
+import com.example.todo.util.onQueryTextChanged
 import com.example.todo.viewModels.TaskViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -33,6 +38,41 @@ class TasksFragment : Fragment(R.layout.fragment_tasks) {
 
         viewModel.tasks.observe(viewLifecycleOwner) {
             taskAdapter.submitList(it)
+        }
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_fragment_task, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.onQueryTextChanged {
+            viewModel.searchQuery.value = it
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_sort_by_name -> {
+
+                true
+            }
+            R.id.action_sort_by_date_created -> {
+
+                true
+            }
+            R.id.action_hide_completed_tasks -> {
+                item.isChecked = !item.isChecked
+
+                true
+            }
+            R.id.action_delete_all_completed_tasks -> {
+
+                true
+            } else -> super.onOptionsItemSelected(item)
         }
     }
 
